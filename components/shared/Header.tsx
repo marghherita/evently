7d226ba5a177ad1e5229b0c93027ace23f2958a3
“marghherita” <margherita.grasso3@gmail.com>
2023-12-20 12:59:42 +0100

    feat. nav bar mobile and desktop

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 function Header() {
   return (
@@ -17,10 +19,18 @@ function Header() {
           />
         </Link>
 
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
+            <MobileNav />
           </SignedIn>
+
           <SignedOut>
             <Button asChild className="rounded-full" size="lg">
               <Link href="/sign-in">Log In</Link>
